d72dc04cd4a3e2f81929900f084f3ef8a942cd63
Prashant Singh <prashanttsinghh205@gmail.com>
2025-11-06 23:50:30 +0530

    Delete fix-all-passwords.sql

--- fix-all-passwords.sql
+++
@@ -1,7 +0,0 @@
--- Fix all passwords with valid bcrypt hashes
--- Admin password: admin123
-UPDATE users SET password = '$2a$10$1D6.4s9Po7Y2NOhxx7ODnu2S1mjTrGCiuKIHqWiiLXynuDEcMQRhC' WHERE email = 'admin@example.com';
-
--- Bob Hostel password: student123  
-UPDATE users SET password = '$2a$10$BW/2hCy.ce/0Ikkgh2FFFexRtT/uOhUMbpkszOVS2sQct1zF5KylK' WHERE email = 'bob.hostel@example.com';
-
